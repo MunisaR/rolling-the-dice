ac6d4b7d04e4b394a29a9c7ce0bf3abb519d1119
MunisaR <munisrahmatullayeva04@gmail.com>
2022-02-17 19:07:09 +0500

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const score0El = document.querySelector('#score--0')
+const score1El = document.querySelector('#score--1')
+
+score0El.textContent = '0'
+score0El.textContent = '0'
+
