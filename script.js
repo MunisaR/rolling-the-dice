c670d25fbd6b0bc652e238d18dfbf6d2d296c835
MunisaR <munisrahmatullayeva04@gmail.com>
2022-03-13 20:18:17 +0500

    commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //Selecting elements
 let score0El = document.getElementById('score--0')
 let score1El = document.getElementById('score--1')
+const diceEl = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
@@ -14,6 +15,7 @@ diceEl.classList.add('hidden')
 //Rolling dice functionality
 btnHold.addEventListener('click', function(){
     //1. Generate a random dice rol 
+
 
 
     //2. Display dice
